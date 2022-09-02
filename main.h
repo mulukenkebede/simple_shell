e26cc7b1a1821f60d750dd50f2aea70c9025cecf
mulukenkebede <eliyas.muluken@gmail.com>
2022-09-02 04:52:43 -0700

    sting operations

---
+++ main.h
@@ -0,0 +1,16 @@
+#ifndef MAIN_H
+#define MAIN_H
+
+#include <stdio.h>
+#include <stdlib.h>
+#include <stdbool.h>
+#include <sys/wait.h>
+#include <unistd.h>
+#include <sys/types.h>
+#include <sys/stat.h>
+#include <fcntl.h>
+#include <errno.h>
+#include <signal.h>
+#include <limits.h>
+
+#endif
